7f1e5d4b9c0fa3ebd1b5bc2075e40b40c2f9ae57
LCN <1455476411@qq.com>
2018-06-18 13:55:09 +0800

    路由

---
+++ src/views/Index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>标题</h1>
+        <router-link tag="span" to="/login">登录</router-link>
+        <div></div>
+        <router-link tag="span" to="/register">注册</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped lang="scss" type="text/css">
+
+</style>
